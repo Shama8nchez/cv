0e5d7eff754c683d973b1e0680a2bfb95541bae9
Shama8nchez <shamanouski@gmail.com>
2023-03-06 14:47:22 +0300

    feat: add popup

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const items = document.querySelectorAll('.experience__content_item');
+
+items.forEach((item) => {
+  item.addEventListener('mouseover', () => {
+    const title = item.querySelector('.experience__content_title');
+    title.classList.add('experience__content_title-active');
+  });
+
+  item.addEventListener('mouseout', () => {
+    const title = item.querySelector('.experience__content_title');
+    title.classList.remove('experience__content_title-active');
+  });
+});
+
+const certPre = document.getElementById('cert-pre');
+const popup = document.querySelector('.popup');
+const certContainer = document.querySelector('.certificate_container');
+
+certPre.addEventListener('click', () => {
+  popup.classList.add('popup-active');
+  certContainer.innerHTML = '<img src="./img/cert1.JPG" alt="certificate" class="certificate">';
+});
+
+popup.addEventListener('click', (e) => {
+  const cert = document.querySelector('.certificate')
+  if (e.target !== cert) {
+    popup.classList.remove('popup-active');
+  }
+});
